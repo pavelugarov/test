895f21de1f2dd8e210bd1c8b4c06ca09ccfe5a40
pavelugarov <49477895+pavelugarov@users.noreply.github.com>
2019-04-10 12:35:08 +0300

    Add files via upload

---
+++ find.php
@@ -0,0 +1,24 @@
+<?php  
+header('Content-type: text/html; charset=windows-1251');
+$str = urldecode($_GET['string']);
+$str = iconv("UTF-8", "WINDOWS-1251", $str);
+echo getPalindromes($str);
+function getPalindromes($str) {
+	$result = null;
+	$minStringLengthPalindrom = 3;
+	$maxStringLengthPalindrom = strlen($str);
+	for ($s=$minStringLengthPalindrom; $s<=$maxStringLengthPalindrom; $s++) {
+		for ($i=0; $i<strlen($str)-$s+1; $i++) {
+			$curString = substr($str, $i, $s);
+			if (isPalindrom($curString)) {
+				$result .= "/$curString"; 				
+			}
+		}
+	}
+	if (is_null($result)) $result = "Нет палиндромов";
+	return $result;
+}
+function isPalindrom($str) {
+	return strcmp(strrev($str), $str) == 0; 
+}	
+?>
